fe255d86d078a15b24133f933a7b9a3e3f13f51e
mhko <ifre01@naver.com>
2023-12-14 22:27:21 +0900

    commit

--- script.js
+++ script.js
@@ -2,39 +2,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     // DOM이 로드된 후 실행되는 코드
 
-    // RSVP 버튼 클릭 시 애니메이션 및 효과 적용
+    // RSVP 버튼 클릭 시 "감사합니다" 문구 표시 및 애니메이션 적용
     const rsvpButton = document.getElementById("rsvp-button");
     const rsvpForm = document.getElementById("rsvp-form");
-    const thankYouMessage = document.getElementById("thank-you-message");
+    const thankYouContainer = document.getElementById("thank-you-container");
 
-    if (rsvpButton && rsvpForm && thankYouMessage) {
+    if (rsvpButton && rsvpForm && thankYouContainer) {
         rsvpButton.addEventListener("click", function () {
+            // RSVP 폼 숨기기
+            rsvpForm.style.display = "none";
+
+            // "감사합니다" 문구 동적으로 생성
+            const thankYouMessage = document.createElement("div");
+            thankYouMessage.id = "thank-you-message";
+            thankYouMessage.textContent = "Thank you for RSVPing!";
+
+            // 문구를 컨테이너에 추가
+            thankYouContainer.appendChild(thankYouMessage);
+
             // 애니메이션 적용
             anime({
-                targets: rsvpForm,
+                targets: thankYouMessage,
                 opacity: 1,
                 translateY: [-50, 0],
                 duration: 1000,
                 easing: "easeInOutQuad"
-            });
-        });
-
-        // RSVP 폼 제출 시 처리
-        rsvpForm.addEventListener("submit", function (event) {
-            event.preventDefault();
-
-            // 애니메이션 적용
-            anime({
-                targets: rsvpForm,
-                opacity: 0,
-                translateY: [0, -50],
-                duration: 1000,
-                easing: "easeInOutQuad",
-                complete: function () {
-                    // 감사 메시지 표시
-                    rsvpForm.style.display = "none";
-                    thankYouMessage.style.display = "block";
-                }
             });
         });
     }
