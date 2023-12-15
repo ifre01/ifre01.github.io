51f82a5e9bb52e1ff52eb63d731c229df4496e6a
mhko <mhko@gsitm.com>
2023-12-15 15:39:36 +0900

    commit

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
 // script.js
+
 document.addEventListener("DOMContentLoaded", function () {
-    // DOM이 로드된 후 실행되는 코드
+    var rsvpButton = document.getElementById("rsvp-button");
+    var closePopupButton = document.getElementById("close-popup");
+    var rsvpForm = document.getElementById("rsvp-form");
 
-    // RSVP 버튼 클릭 시 "감사합니다" 문구 표시 및 애니메이션 적용
-    const rsvpButton = document.getElementById("rsvp-button");
-    const rsvpForm = document.getElementById("rsvp-form");
-    const thankYouContainer = document.getElementById("thank-you-container");
-    
-    if (rsvpButton && rsvpForm && thankYouContainer) {
-        rsvpButton.addEventListener("click", function () {
-            // RSVP 폼 숨기기
-            rsvpForm.style.display = "none";
-            thankYouContainer.style.display = "block";
-            
-            // "감사합니다" 문구 동적으로 생성
-            const thankYouMessage = document.createElement("div");
-            thankYouMessage.id = "thank-you-message";
-            thankYouMessage.textContent = "Thank you for RSVPing!";
-
-            // 문구를 컨테이너에 추가
-            thankYouContainer.appendChild(thankYouMessage);
-
-            // 애니메이션 적용
-            anime({
-                targets: thankYouMessage,
-                opacity: 1,
-                translateY: [-50, 0],
-                duration: 1000,
-                easing: "easeInOutQuad"
-            });
-        });
+    function openPopup() {
+        rsvpForm.style.display = "block";
     }
+
+    function closePopup() {
+        rsvpForm.style.display = "none";
+    }
+
+    rsvpButton.addEventListener("click", openPopup);
+    closePopupButton.addEventListener("click", closePopup);
 });
